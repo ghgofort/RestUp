ba51265aac9e2c6e78d2c88c13c03b8fc32d65a4
ghgofort <ghgofort@asu.edu>
2016-07-16 17:40:12 -0700

    Business Models added

---
+++ BusinessEntities/BasketItemEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntities
+{
+    class BasketItemEntity
+    {
+        public int BasketItemId { get; set; }
+        public string ItemName { get; set; }
+        public int BasketId { get; set; }
+        public byte[] BidTime { get; set; }
+    }
+}
